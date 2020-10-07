f1d7e643039d512cd142867a1237148baeea7190
Jen E <jenevans3ten@gmail.com>
2020-10-07 12:24:31 -0400

    starter MVC web app with test project and gitignore

---
+++ cle-fall-2020-courses.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace cle_fall_2020_courses.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
