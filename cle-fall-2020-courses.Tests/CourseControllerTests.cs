5dc8691257543ea17ceed58b16e7722480bc4a54
Jen E <jenevans3ten@gmail.com>
2020-10-08 14:05:17 -0400

    10/8: 2nd course index test, course models test, update index view

--- cle-fall-2020-courses.Tests/CourseControllerTests.cs
+++ cle-fall-2020-courses.Tests/CourseControllerTests.cs
@@ -1,4 +1,5 @@
 using cle_fall_2020_courses.Controllers;
+using cle_fall_2020_courses.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
@@ -7,17 +8,29 @@ namespace cle_fall_2020_courses.Tests
 {
     public class CourseControllerTests
     {
+        CourseController sut;
+        
+        public CourseControllerTests()
+        {
+            sut = new CourseController();
+        }
+
         [Fact]
         public void Index_Returns_A_View()
         {
-            // Arrange
-            CourseController sut = new CourseController();
-
             // Act
             var result = sut.Index();
 
             // Assert
             Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Index_Returns_CourseModel_To_View()
+        {
+            var result = sut.Index();
+
+            Assert.IsType<Course>(result.Model);
         }
     }
 }
---
+++ cle-fall-2020-courses.Tests/CourseTests.cs
@@ -0,0 +1,42 @@
+﻿using cle_fall_2020_courses.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace cle_fall_2020_courses.Tests
+{
+    public class CourseTests
+    {
+        Course sut;
+
+        public CourseTests()
+        {
+            sut = new Course(42, "Course name", "This is the course description");
+        }
+
+        [Fact]
+        public void CourseConstructor_Sets_Id_On_CourseModel()
+        {
+            int result = sut.Id;
+
+            Assert.Equal(42, result);
+        }
+
+        [Fact]
+        public void CourseConstructor_Sets_Name_On_CourseModel()
+        {
+            string result = sut.Name;
+
+            Assert.Equal("Course name", result);
+        }
+
+        [Fact]
+        public void CourseConstructor_Sets_Description_On_CourseModel()
+        {
+            string result = sut.Description;
+
+            Assert.Equal("This is the course description", result);
+        }
+    }
+}
