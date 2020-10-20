f93c03539cd9f57ce88b30dcc0e74f164d4cd37e
Codycoder <andykohler99@gmail.com>
2020-10-20 12:12:04 -0400

    Added migration for new Course LastRevision time property

---
+++ cle-fall-2020-courses/Migrations/20201020155156_LastRevisionTimeStamp.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace cle_fall_2020_courses.Migrations
+{
+    public partial class LastRevisionTimeStamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastRevision",
+                table: "Courses",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LastRevision",
+                value: "12/31/18");
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "LastRevision",
+                value: "12/31/18");
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "LastRevision",
+                value: "12/31/18");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastRevision",
+                table: "Courses");
+        }
+    }
+}
